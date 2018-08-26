da4aa1114eeeaf8f9e7ad0083b5d654aff9a2cf7
lgdd <lg.durand@protonmail.com>
2018-08-26 02:37:49 +0200

    Add navbar shrink animation to freelancer theme

--- startbootstrap-freelancer-theme/src/js/main.js
+++ startbootstrap-freelancer-theme/src/js/main.js
@@ -1,23 +1,47 @@
-AUI().ready(
-  'liferay-sign-in-modal',
-  function(A) {
-    var BODY = A.getBody();
+(function ($) {
+  "use strict"; // Start of use strict
 
-    var signIn = A.one('#sign-in');
+  // Activate scrollspy to add active class to navbar items on scroll
+  $('body').scrollspy({
+    target: '#navigation',
+    offset: 56
+  });
 
-    if (signIn && signIn.getData('redirect') !== 'true') {
-      signIn.plug(Liferay.SignInModal);
+  // Collapse Navbar
+  var navbarCollapse = function() {
+    if ($("#navigation").offset().top > 100) {
+      $("#navigation").addClass("navbar-shrink");
+    } else {
+      $("#navigation").removeClass("navbar-shrink");
     }
+  };
+  // Collapse now if page is not at top
+  navbarCollapse();
+  // Collapse the navbar when page is scrolled
+  $(window).scroll(navbarCollapse);
 
-    var fullScreenToggleIcon = A.one('.full-screen-navigation #banner .navbar-toggler');
+  AUI().ready(
+    'liferay-sign-in-modal',
+    function (A) {
+      var BODY = A.getBody();
 
-    if (fullScreenToggleIcon) {
-      fullScreenToggleIcon.on(
-        'click',
-        function(event) {
-          BODY.toggleClass('main-nav-opened', event.currentTarget.hasClass('collapsed'));
-        }
-      );
+      var signIn = A.one('#sign-in');
+
+      if (signIn && signIn.getData('redirect') !== 'true') {
+        signIn.plug(Liferay.SignInModal);
+      }
+
+      var fullScreenToggleIcon = A.one('.full-screen-navigation #banner .navbar-toggler');
+
+      if (fullScreenToggleIcon) {
+        fullScreenToggleIcon.on(
+          'click',
+          function (event) {
+            BODY.toggleClass('main-nav-opened', event.currentTarget.hasClass('collapsed'));
+          }
+        );
+      }
     }
-  }
-);
+  );
+
+})(jQuery); // End of use strict
